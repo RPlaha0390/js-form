965fae8204be15b9b0d8344b24d38c6b220ddf73
Raman Plaha <ramanplaha1990@gmail.com>
2020-05-16 23:55:03 +0100

    Add pikaday datepicker

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 import './styles/app.scss';
+import Pikaday from 'pikaday';
+
+const picker = new Pikaday({
+  field: document.getElementById('datepicker'),
+  format: 'D MMM YYYY',
+  defaultDate: new Date('8/5/2019'),
+  minDate: new Date('8/5/2019'),
+  maxDate: new Date('9/13/2019'),
+  keyboardInput: true,
+});
